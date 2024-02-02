15a9b705393faf551f8b10ffd630da7dadc2aee7
deus-tll <millerartemka14@gmail.com>
2024-02-02 16:57:37 +0200

    adding route for login

--- backend/api/src/routes/api.php
+++ backend/api/src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('auth/register', RegisterController::class)->name('api.auth.register');
+Route::post('auth/login', LoginController::class)->name('api.auth.login');

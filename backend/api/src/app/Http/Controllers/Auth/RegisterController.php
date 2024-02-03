b5950ed934dccbdd4070aaa68f2273613309925f
deus-tll <millerartemka14@gmail.com>
2024-02-03 18:22:25 +0200

    updating RegisterController

--- backend/api/src/app/Http/Controllers/Auth/RegisterController.php
+++ backend/api/src/app/Http/Controllers/Auth/RegisterController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Http\Resources\Auth\BadRequestResource;
-use App\Http\Resources\Auth\FailedAuthorizationResource;
-use App\Http\Resources\Auth\SuccessAuthResponseResource;
+use App\Http\Resources\Auth\SuccessAuthResource;
+use App\Http\Resources\BaseWithResponseResource;
+use app\Http\Resources\Errors\InternalServerErrorResource;
 use App\Services\Auth\JwtService;
 use App\Services\Auth\UserService;
 use OpenApi\Attributes as OAT;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 #[OAT\Post(
     path: '/api/auth/register',
@@ -24,32 +25,55 @@ use OpenApi\Attributes as OAT;
         new OAT\Response(
             response: 201,
             description: 'Registration was successful',
-            content: new OAT\JsonContent(ref: '#/components/schemas/SuccessAuthResponseResource')
+            content: new OAT\JsonContent(ref: '#/components/schemas/SuccessAuthResource')
         ),
         new OAT\Response(
             response: 400,
-            description: 'Password confirmation failed',
-            content: new OAT\JsonContent(ref: '#/components/schemas/BadRequestResource')
+            description: "The passwords don't match",
+            content: new OAT\JsonContent(
+                properties: [
+                    new OAT\Property(
+                        property: 'status',
+                        type: 'string',
+                        example: 'failure'
+                    ),
+                    new OAT\Property(
+                        property: 'message',
+                        type: 'string',
+                        example: "The passwords don't match"
+                    ),
+                ]
+            )
         ),
+        new OAT\Response(
+            response: 500,
+            description: 'Some internal server error occurred',
+            content: new OAT\JsonContent(ref: '#/components/schemas/InternalServerErrorResource')
+        )
     ]
 )]
 class RegisterController extends Controller
 {
     public function __construct(
         private readonly UserService $userService,
-        private readonly JwtService  $jwtService,
+        private readonly JwtService  $jwtService
     ){}
 
-    public function __invoke(RegisterRequest $request): BadRequestResource|FailedAuthorizationResource|SuccessAuthResponseResource
+    public function __invoke(RegisterRequest $request): SuccessAuthResource|BaseWithResponseResource|InternalServerErrorResource
     {
-        if ($request->input('password') != $request->input('password_confirmation')){
-            return new BadRequestResource("The passwords don't match");
+        try {
+            if ($request->input('password') != $request->input('password_confirmation')){
+                return new BaseWithResponseResource(null, "The passwords don't match", ResponseAlias::HTTP_BAD_REQUEST, 'failure');
+            }
+
+            $this->userService->create($request->validated());
+
+            $token = $this->jwtService->guardApiAttempt($request->only('email', 'password'));
+
+            return $this->jwtService->buildResponse($token, 'Registration was successful', true);
         }
-
-        $this->userService->create($request->validated());
-
-        $token = $this->jwtService->guardApiAttempt($request->only('email', 'password'));
-
-        return $this->jwtService->buildResponse($token, 'Registration was successful', true);
+        catch (\Exception $e) {
+            return new InternalServerErrorResource(['error' => $e->getMessage()]);
+        }
     }
 }

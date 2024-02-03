bc58d45f1cbd828d18f595e9b350a00e8080ac4c
deus-tll <millerartemka14@gmail.com>
2024-02-03 18:24:20 +0200

    creating routes for ProfileController, RefreshTokenController and LogoutController

--- backend/api/src/routes/api.php
+++ backend/api/src/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ProfileController;
+use App\Http\Controllers\Auth\RefreshTokenController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +25,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', RegisterController::class)->name('api.auth.register');
 Route::post('auth/login', LoginController::class)->name('api.auth.login');
+
+Route::group([
+    'middleware' => ['auth:api']
+], function() {
+    Route::get('auth/profile', [ProfileController::class, 'profile'])->name('api.auth.profile');
+    Route::get('auth/refresh-token', [RefreshTokenController::class, 'refreshToken'])->name('api.auth.refreshToken');
+    Route::get('auth/logout', [LogoutController::class, 'logout'])->name('api.auth.logout');
+});

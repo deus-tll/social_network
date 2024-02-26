8bee1c975c894f35c9368a08756c5a3410de16ff
deus-tll <millerartemka14@gmail.com>
2024-02-26 20:18:16 +0200

    update

--- frontend/public/src/src/providers/socket/socketHandlers.js
+++ frontend/public/src/src/providers/socket/socketHandlers.js
@@ -19,5 +19,7 @@ export const handlePing = (data) => {
 export const handleAvatarsStored = (data) => {
   const avatars = JSON.parse(data);
   console.log('Received avatars:', avatars);
+
+  //тут я розумію що воно не спрацює, бо потрібно через dispatch викликати, але воно в цілому поки не вдається вивести в консоль Received avatars хоча б.
   setUserAvatars({avatars});
 };
